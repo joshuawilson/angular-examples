f7066c5ca2edf1146c4ae2c8407e8658354c917b
joshuawilson <javajoshw@gmail.com>
2016-03-12 21:13:35 -0500

    complete the steps needed to get the Service working and using a Promise in the quickstart

--- quickstart/app/app.component.ts
+++ quickstart/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
 import {Hero} from './hero';
 import {HeroDetailComponent} from './hero-detail.component';
 import {HeroService} from './hero.service';
@@ -9,7 +9,9 @@ import {HeroService} from './hero.service';
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
-        <li *ngFor="#hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
+        <li *ngFor="#hero of heroes"
+            (click)="onSelect(hero)"
+            [class.selected]="hero === selectedHero">
             <span class="badge">{{hero.id}}</span> {{hero.name}}
         </li>
     </ul>
@@ -67,12 +69,21 @@ import {HeroService} from './hero.service';
     directives: [HeroDetailComponent],
     providers: [HeroService]
 })
-export class AppComponent {
-    constructor(private _heroService: HeroService) { }
-
+export class AppComponent implements OnInit {
     title = 'Tour of Heroes';
-    heroes = Hero[];
+    heroes: Hero[];
     selectedHero: Hero;
 
+    constructor(private _heroService: HeroService) { }
+
+    getHeroes() {
+        this._heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+
+    ngOnInit() {
+        this.getHeroes();
+    }
+
     onSelect(hero: Hero) { this.selectedHero = hero; }
+
 }
