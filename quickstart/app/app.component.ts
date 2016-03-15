bf46c785a29ffb294bd71872666b3a2d89897353
joshuawilson <javajoshw@gmail.com>
2016-03-14 21:42:04 -0400

    add ability to simulate a delay in the Quickstart

--- quickstart/app/app.component.ts
+++ quickstart/app/app.component.ts
@@ -6,16 +6,16 @@ import {HeroService} from './hero.service';
 @Component({
     selector: 'my-app',
     template:`
-    <h1>{{title}}</h1>
-    <h2>My Heroes</h2>
-    <ul class="heroes">
-        <li *ngFor="#hero of heroes"
-            (click)="onSelect(hero)"
-            [class.selected]="hero === selectedHero">
-            <span class="badge">{{hero.id}}</span> {{hero.name}}
-        </li>
-    </ul>
-    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+        <h1>{{title}}</h1>
+        <h2>My Heroes</h2>
+        <ul class="heroes">
+            <li *ngFor="#hero of heroes"
+                (click)="onSelect(hero)"
+                [class.selected]="hero === selectedHero">
+                <span class="badge">{{hero.id}}</span> {{hero.name}}
+            </li>
+        </ul>
+        <my-hero-detail [hero]="selectedHero"></my-hero-detail>
     `,
     styles:[`
       .selected {
--- quickstart/app/hero.service.ts
+++ quickstart/app/hero.service.ts
@@ -6,4 +6,10 @@ export class HeroService {
     getHeroes() {
         return Promise.resolve(HEROES);
     }
+    // See the "Take it slow" appendix
+    getHeroesSlowly() {
+        return new Promise<Hero[]>(resolve =>
+            setTimeout(()=>resolve(HEROES), 2000) // 2 seconds
+        );
+    }
 }
